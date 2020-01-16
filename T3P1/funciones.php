2fbaeb57d190cee229cbfacbb21a5bdca16783f4
gonzalo.ruiz <56383879+gonzaloruizalonso@users.noreply.github.com>
2020-01-16 13:42:06 +0100

    Update funciones.php

--- T3P1/funciones.php
+++ T3P1/funciones.php
@@ -1,6 +1,6 @@
 <?php
 function conectar(){
-  $servername = stripslashes(htmlspecialchars(trim('localhost')));
+  $servername = stripslashes(htmlspecialchars(trim('10.131.1.199')));
   $username = stripslashes(htmlspecialchars(trim('root')));
   $password = stripslashes(htmlspecialchars(trim('rootroot')));
   $dbname = stripslashes(htmlspecialchars(trim('empleadosnm')));
