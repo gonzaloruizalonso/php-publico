4149b03722597d351c1583f792426a77d5f22531
gonzalo.ruiz <56383879+gonzaloruizalonso@users.noreply.github.com>
2020-01-17 13:00:12 +0100

    Update conexion.php

--- webcompras/conexion.php
+++ webcompras/conexion.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', '10.131.1.199');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
-define('DB_DATABASE', 'comprasweb');
+define('DB_DATABASE', 'COMPRASWEB');
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
    if (!$conn) {
